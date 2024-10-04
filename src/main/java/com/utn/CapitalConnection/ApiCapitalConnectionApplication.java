8dc39ce57af5c225d153a742aa2f6e9d7872247b
Julian Pellegrini <pellegrinijulianmauro@gmail.com>
2024-10-04 15:26:46 -0300

    Base 0

---
+++ src/main/java/com/utn/CapitalConnection/ApiCapitalConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.utn.CapitalConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCapitalConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCapitalConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/CapitalConnection/ApiCapitalConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.CapitalConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCapitalConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
